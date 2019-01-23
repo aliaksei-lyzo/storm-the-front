e51f1cd5564e608c8422b709bd7de0981630fde1
Maryia Krauchanka <marikravs@mail.ru>
2019-01-23 22:57:36 +0300

    router setup, suspense feature, basic routes and links

---
+++ src/components/AppContainer/AppContainer.jsx
@@ -0,0 +1,27 @@
+import React, { Component, Suspense } from 'react';
+import { Provider } from 'react-redux';
+import { hot } from 'react-hot-loader';
+import { BrowserRouter } from 'react-router-dom';
+
+import configureStore from 'config/store';
+import routes from 'config/routes';
+import App from 'components/App';
+
+const store = configureStore();
+
+class AppContainer extends Component {
+  render() {
+    return (
+      <Provider store={store}>        
+          <BrowserRouter>
+            <App>{routes}</App>
+          </BrowserRouter>        
+      </Provider>
+    );
+  }
+}
+
+/* propTypes declaration */
+AppContainer.propTypes = {};
+
+export default hot(module)(AppContainer);
---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const LandingPage = () => <div>LANDING PAGE</div>;
+
+/* propTypes declaration */
+LandingPage.propTypes = {};
+
+export default LandingPage;
