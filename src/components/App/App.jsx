04793a7e45ec4c57e728dbfe1abba128bbfbfe96
yan.tsitulenka.epam <yan_tsitulenka@epam.com>
2019-02-07 14:58:15 +0300

    Added footer component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader';
 import { connect } from 'react-redux';
 import * as actions from 'actions/items';
+import Footer from '../Footer'
 
+import 'stylesheet/main.scss';
 import style from './App.scss';
 
 const App = ({ items, addItem, addItemAsync }) => (
-  <div className={style.app}>
-    <button type="button" onClick={addItem.bind(this, { id: Math.random() })}>submit sync test action to store</button>
-    <button type="button" onClick={addItemAsync.bind(this, { id: Math.random() })}>submit async test action to store</button>
-    {items.map(item => <p key={item.id}>{item.id}</p>)}
-  </div>
+  <React.Fragment>
+    <main className={style.content}>
+      <button type="button" onClick={addItem.bind(this, { id: Math.random() })}>submit sync test action to store</button>
+      <button type="button" onClick={addItemAsync.bind(this, { id: Math.random() })}>submit async test action to store</button>
+      {items.map(item => <p key={item.id}>{item.id}</p>)}
+    </main>
+    <Footer/>
+  </React.Fragment>
 );
 
 /* propTypes declaration */
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './Footer.scss';
+
+const Footer = () => (
+  <footer className={style.footer}>
+    some information
+  </footer>
+);
+
+export default Footer;
